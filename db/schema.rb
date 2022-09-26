e759afb1c9e3aad2077bdc79eb46017dbd70299c
alvo254 <alvinkhamron@gmail.con>
2022-09-26 21:15:57 +0300

    using scaffold to create crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_26_163702) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_181350) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -25,6 +25,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_163702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
